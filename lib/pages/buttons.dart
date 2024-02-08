9b053c8071f2eed1199b8adb2336d494e78ed11f
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-08 19:43:40 +0530

    covid frontend

--- lib/pages/buttons.dart
+++ lib/pages/buttons.dart
@@ -15,90 +15,9 @@ class buttons extends StatelessWidget {
 
 
         ),
-        body: SingleChildScrollView(
-          child: Container(
-
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.pink.withOpacity(.2),
-                        Colors.white.withOpacity(.2),
-                        Colors.cyanAccent.withOpacity(.2),
-                        Colors.pink.withOpacity(.2),
-                      ]
-                  )
-              ),
-
-              padding: EdgeInsets.all(50),
-              child: Column(
-                children: [
-                  SizedBox(height: 350, width: 850),
-                SizedBox(
-                  height: 60,
-                  width: 200,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black12.withOpacity(.2),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero
-                            )
-                        ),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>add()));
-
-
-                        },
-                        child: Text("ADD PATIENT")),
-                  ),
-
-                  SizedBox(height: 20,width: 250,),
-                  SizedBox(
-                    height: 60,
-                    width: 200,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black12.withOpacity(.2),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero
-                            )
-                        ),
-                        onPressed: () {
-
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>search()));
 
 
 
-                        },
-                        child: Text("SEARCH PATIENT")),
-                  ),
-
-                  SizedBox(height: 20,width: 250,),
-                  SizedBox(
-                    height: 60,
-                    width: 200,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black12.withOpacity(.2),
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero
-                            )
-                        ),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>postlist()));
-
-
-                        },
-                        child: Text("VIEW PATIENTS")),
-                  ),
-
-
-                ],
-              )
-          ),
-        ),
       ),
 
     );
