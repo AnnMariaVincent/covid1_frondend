6ad399809d96faaf16e1ad56fe2c7730dc940b2e
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-08 20:48:44 +0530

    Patients  List

--- lib/pages/postlist.dart
+++ lib/pages/postlist.dart
@@ -1,9 +1,22 @@
+import 'package:covidapplication/model/postmodel.dart';
+import 'package:covidapplication/service/postservice.dart';
 import 'package:flutter/material.dart';
 
-class postlist extends StatelessWidget {
+class postlist extends StatefulWidget {
   const postlist({super.key});
 
   @override
+  State<postlist> createState() => _postlistState();
+}
+
+class _postlistState extends State<postlist> {
+  Future<List<Patient>> ? data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    data=PostApiService().getPatient();
+  }
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -11,6 +24,37 @@ class postlist extends StatelessWidget {
           backgroundColor: Colors.pinkAccent,
 
         ),
+        body: FutureBuilder(future: data, builder: (context,sshot){
+          if(sshot.hasData && sshot.data!.isNotEmpty)
+          {
+            return ListView.builder(
+                itemCount: sshot.data!.length,
+                itemBuilder: (value,index){
+                  return Card(
+                    child: Column(
+                      children: [
+                        ListTile(
+
+                          title: Text("PATIENT NAME:" +(sshot.data![index].name.toString()) ),
+                          subtitle: Text("ADDRESS:" +(sshot.data![index].address.toString()) + "\n" +"PHONE NUMBER :" + (sshot.data![index].phone.toString())+"\n"+"SYMPTOMS" + (sshot.data![index].symptoms.toString())+"\n"+"STATUS" + (sshot.data![index].status.toString())),
+
+                        ),
+                      ],
+                    ),
+                  );
+
+                });
+          }
+          else
+          {
+            return Text("Loading");
+          }
+        }),
+
+
+
+
+
       ),
     );
   }
