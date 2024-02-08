8ba999de155e59c6362213a86edfea8c2f322ae6
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-08 21:10:48 +0530

    Patient Add

--- lib/service/postservice.dart
+++ lib/service/postservice.dart
@@ -1,6 +1,41 @@
+import 'dart:convert';
+
 import 'package:covidapplication/model/postmodel.dart';
+import 'package:covidapplication/pages/addpatient.dart';
 import 'package:http/http.dart' as http;
 class PostApiService{
+
+
+  Future<dynamic> sendbutton(String name,address,phone,symp,status) async {
+    var client = http.Client();
+    var apiUrl = Uri.parse("http://localhost:3001/api/patient/add");
+    var response = await client.post(apiUrl,
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: jsonEncode(<String, String>{
+
+            "name":name,
+            "phone":phone,
+            "address":address,
+            "symptoms":symp,
+            "status":status,
+        }
+        )
+    );
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    else {
+      throw Exception("Failure");
+    }
+  }
+
+
+
+
+
+
   Future<List<Patient>> getPatient() async{
     var client=http.Client();
     var apiUrl=Uri.parse("http://localhost:3001/api/patient/viewall");
